3acd49363dd9d620378e6c10c260321529e4c377
Nicolas Cloutier <nic007nc@gmail.com>
2017-02-02 19:41:48 -0500

    Added a missing file and included Json.Net for future use

---
+++ CSA/RoslynWalkers/ConsoleDumpWalker.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace CSA.RoslynWalkers
+{
+    internal class ConsoleDumpWalker : SyntaxWalker
+    {
+        public override void Visit(SyntaxNode node)
+        {
+            int padding = node.Ancestors().Count();
+            //To identify leaf nodes vs nodes with children
+            string prepend = node.ChildNodes().Any() ? "[-]" : "[.]";
+            //Get the type of the node
+            string line = new String(' ', padding) + prepend +
+                          " " + node.GetType().ToString();
+            //Write the line
+            System.Console.WriteLine(line);
+            base.Visit(node);
+        }
+
+    }
+}
